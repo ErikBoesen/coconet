d9c1bf53f086769efcf28e6dd4035187987ab952
ErikBoesen <me@erikboesen.com>
2017-06-18 12:36:35 -0400

    Merge fill.sh and fillinux.sh

--- apps/fill.sh
+++ apps/fill.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+rhash() {
+    if [ `uname` == "Darwin" ]; then echo $RANDOM | md5
+    else echo $RANDOM | md5sum | awk '{print $1}'; fi
+}
+
 for ((i = 1;; i++)); do
     echo "Getting photo $i..."
-    curl -s "https://www.fillmurray.com/$((100+$RANDOM%2000))/$((100+$RANDOM%2000))" --output $(echo $RANDOM | md5).jpg
+    curl -s "https://www.fillmurray.com/$((100+$RANDOM%2000))/$((100+$RANDOM%2000))" --output "$(rhash).jpg"
 done
--- apps/fillinux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ((i = 1;; i++)); do
-    echo "Getting photo $i..."
-    curl -s "https://www.fillmurray.com/$((100+$RANDOM%2000))/$((100+$RANDOM%2000))" --output "$(echo $RANDOM | md5sum).jpg"
-done
