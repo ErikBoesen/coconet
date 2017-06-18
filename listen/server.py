c292643a98bc47fdae9f6228741624c7e81efb58
ErikBoesen <me@erikboesen.com>
2017-06-17 22:07:45 -0400

    Clean up and tweak listener script

--- listen/server.py
+++ listen/server.py
@@ -1,27 +1,30 @@
-#!/usr/bin/python3
-
-import os
 import socket
 
-ME = 'boesene'
+OWNER = 'boesene'
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname()  # Get local machine name
-port = 2043                 # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+s = socket.socket()
+host = socket.gethostname()
+port = 2043
+s.bind((host, port))
 
 print('Listening on port %s...' % port)
 s.listen(5)
 while True:
-    c, addr = s.accept()    # Establish connection with client.
-    print('Got connection from %s:%s' % addr)
+    # Establish new connection
+    c, addr = s.accept()
+    # Store recieved data
     data = c.recv(1024).decode()
-    print('New node online: %s' % data, end='')
-    if ME not in data:  # Don't bother saving the data if it's just me testing
-        with open('nodes.txt', 'a+') as f:
-            f.write(data)
-
-    resp = '[SERVER] Node %s accepted.' % data[:-1].replace(' ', '@')
-    # We need to trim of the last newline character
-    c.send(resp.encode())
-    c.close()  # Close the connection
+    # Filter raw HTTP requests
+    if data.count('\n') <= 1:
+        # Ignore join attempts from owner
+        if OWNER not in data:
+            print('Node has joined: %s' % data, end='')
+            # Write node data to file
+            with open('nodes.txt', 'a+') as f:
+                f.write(data)
+        else:
+            print('Ignoring join attempt from node identified as owner\'s computer.')
+        # Send response once node data has been stored
+        c.send('[SERVER] Node accepted, may hibernate.'.encode())
+    # Close the connection
+    c.close()
