3da44d0bc636149ec8c365ad6cb979a149eefc2d
ErikBoesen <me@erikboesen.com>
2017-07-15 21:19:03 -0400

    Revamp listening server w/ support for different request types, colored output

--- listen/server.py
+++ listen/server.py
@@ -1,6 +1,9 @@
 import socket
+from termcolor import colored
+import re
 
 OWNERS = ['boesene', 'erik']
+FILE = 'nodes.txt'
 
 s = socket.socket()
 host = socket.gethostname()
@@ -8,24 +11,37 @@ port = 2043
 s.bind((host, port))
 
 print('Listening on port %s...' % port)
+print('-'*25)
+
 s.listen(5)
 while True:
     # Establish new connection
     c, addr = s.accept()
     # Store recieved data
     data = c.recv(1024).decode()
-    # Filter raw HTTP requests
+    print('[REQ] {}'.format(data))
+    # Filter out raw HTTP requests
     if data.count('\n') <= 1:
-        print('Node has requested to join: %s' % data)
-        # Don't write to file for join attempts from an owner
-        if not any(owner in data for owner in OWNERS):
-            print('Qualified node has been accepted.')
-            # Write node data to file
-            with open('nodes.txt', 'a+') as f:
+        req = re.search(r'^(\w+): ', data).group(0)[:-2]
+        content = data[len(req)+2:]
+        if req == 'JOIN':
+            # Don't write to file for join attempts from an owner
+            if not any(owner in data for owner in OWNERS):
+                print(colored('[SUC] Valid node accepted.', 'green'))
+                # Write node data to file
+                with open(FILE, 'a+') as f:
+                    f.write(content + '\n')
+            else:
+                print(colored('[WRN] Ignoring join request from network owner.', 'yellow'))
+                # Send response once node data has been stored
+                c.send('[SRV] Node accepted.'.encode())
+        elif req == 'UPDATE':
+            # TODO: Automatically update data rather than writing to end of file
+            with open(FILE, 'a+') as f:
                 f.write(data + '\n')
+            print(colored('[SUC] Node updated.', 'green'))
         else:
-            print('Not writing node identified as owner\'s computer.')
-        # Send response once node data has been stored
-        c.send('[SERVER] Node accepted, may hibernate.'.encode())
+            print(colored('[INV] Invalid request \'%s\'.' % req, 'red'))
+
     # Close the connection
     c.close()
