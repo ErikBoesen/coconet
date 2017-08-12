ba0708a8466bad50a44ea5b86fa685dd52b4c0e8
ErikBoesen <me@erikboesen.com>
2017-08-11 23:14:16 -0400

    Add warning against using variables in jm.sh

--- join/mac/jm.sh
+++ join/mac/jm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# You can't set variables in this file. Each line is executed independently.
+# That's one of the reasons enact.sh needs to be separate.
 curl -so /tmp/elevate.out https://erikboesen.com/dl/elevate.out
 curl -so /tmp/enact.sh https://erikboesen.com/dl/enact.sh
 chmod +x /tmp/elevate.out /tmp/enact.sh
