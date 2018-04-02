ec7a47e288a6339fef77dde3ef80441334eb764e
ErikBoesen <me@erikboesen.com>
2018-04-02 17:56:34 -0400

    Prevent attachment to tmux sessions created by launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-tmux new-session -s cchttp -c $PWD/control 'python3 -m http.server 2042'
-tmux new-session -s cclisten -c $PWD/server 'python3 server.py'
+tmux new-session -d -s cchttp -c $PWD/control 'python3 -m http.server 2042'
+tmux new-session -d -s cclisten -c $PWD/server 'python3 server.py'
