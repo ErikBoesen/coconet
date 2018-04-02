d361441f17e9e6278cdc2bc845a939e631ec9816
ErikBoesen <me@erikboesen.com>
2018-04-02 17:46:17 -0400

    Change tmux start directory rather than running cd

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-tmux new-session -s cchttp 'cd control && python3 -m http.server 2042'
-tmux new-session -s cclisten 'cd server && python3 server.py'
+tmux new-session -s cchttp -c $PWD/control 'python3 -m http.server 2042'
+tmux new-session -s cclisten -c $PWD/server 'python3 server.py'
