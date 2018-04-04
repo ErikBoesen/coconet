9ce0426d8292b390337e21f93d4c488b2fef3b3d
ErikBoesen <me@erikboesen.com>
2018-04-04 18:31:17 -0400

    Add support for starting ccrsh session in launch.sh

--- launch.sh
+++ launch.sh
@@ -2,3 +2,6 @@
 
 tmux new-session -d -s cchttp -c $PWD/control 'python3 -m http.server 2042'
 tmux new-session -d -s cclisten -c $PWD/server 'python3 server.py'
+if [ "$1" = "--rsh" ] || [ "$1" = "-r" ]; then
+    tmux new-session -d -s ccrsh 'nc -l -p 2044 -vvv'
+fi
